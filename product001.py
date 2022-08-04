2f4395f0e5dfe59584896136fdbb1f551682b3fd
Soldi99 <103605443+Soldi99@users.noreply.github.com>
2022-08-03 22:13:40 -0700

    vers_0.0.1

---
+++ product001.py
@@ -0,0 +1,24 @@
+# enter
+passwords = []
+users = []
+print("Bienvenido a AcSystem")
+print('''Por favor, selecciona una opción:
+a) Ingresar
+b) Registrarme''')
+option = input('Oprime la tecla: ')
+if option == 'a' or 'A':
+    print("Usted es un usuario con una cuenta existente, por favor dígite su usuario y contraseña")
+    user = input('User: ')
+    password = input('Password: ')
+    while password not in passwords and user not in users:
+        print('Usuario y/o contraseña incorrecta, digíte nuevamente')
+        user = input('User: ')
+        password = input('Password: ')
+if option == 'b' or 'B':
+    print('Procederemos con el registro de su usuario en nuestra base de datos')
+    a = input('Ingresa tu usuario: ')
+    users.append(a)
+    b = input('Ingresa tu contraseña: ')
+    passwords.append(b)
+print(users)
+print(passwords)
